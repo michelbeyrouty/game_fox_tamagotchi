0fd3c216898e2bb9053b9a3e8b38ec4735db684d
michelbeyrouty <michelbeyrouty@gmail.com>
2023-05-02 22:07:10 -0400

    Add buttons

--- src/init.js
+++ src/init.js
@@ -1,15 +1,17 @@
-import gameState from "./gameState"
-
-const TICK_RATE = 3000;
+import game from "./gameState"
+import initButtons from "./bottons";
+import { TICK_RATE } from "./constants"
 
 async function init() {
   console.log("starting game");
+
+  initButtons(game.handleUserAction)
 
   let nextTimeToTick = Date.now();
   function nextAnimationFrame() {
     const now = Date.now();
     if (nextTimeToTick <= now) {
-      gameState.tick();
+      game.tick;
       nextTimeToTick = now + TICK_RATE;
     }
     requestAnimationFrame(nextAnimationFrame);
